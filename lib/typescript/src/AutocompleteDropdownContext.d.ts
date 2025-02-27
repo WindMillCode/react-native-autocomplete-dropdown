5dbe5894d06cfb7829ec1a213503287c60c578e8
windmillcode0 <michaelodumosu57@gmail.com>
2025-02-27 09:40:22 -0500

    [UPDATE] additional work

---
+++ lib/typescript/src/AutocompleteDropdownContext.d.ts
@@ -0,0 +1,22 @@
+import React from 'react';
+import type { SetStateAction, Dispatch, FC, ReactElement, MutableRefObject } from 'react';
+import { View } from 'react-native';
+import type { IAutocompleteDropdownRef } from './types';
+export interface IAutocompleteDropdownContext {
+    content?: ReactElement;
+    setContent: Dispatch<SetStateAction<ReactElement | undefined>>;
+    direction?: 'up' | 'down';
+    setDirection: Dispatch<SetStateAction<IAutocompleteDropdownContext['direction']>>;
+    activeInputContainerRef?: MutableRefObject<View | null>;
+    activeControllerRef?: MutableRefObject<IAutocompleteDropdownRef | null>;
+    controllerRefs?: MutableRefObject<IAutocompleteDropdownRef[]>;
+    closeOnTouchEnd?: boolean;
+}
+export interface IAutocompleteDropdownContextProviderProps {
+    headerOffset?: number;
+    children: React.ReactNode;
+    closeOnTouchEnd?: boolean;
+}
+export declare const AutocompleteDropdownContext: React.Context<IAutocompleteDropdownContext>;
+export declare const AutocompleteDropdownContextProvider: FC<IAutocompleteDropdownContextProviderProps>;
+//# sourceMappingURL=AutocompleteDropdownContext.d.ts.map
