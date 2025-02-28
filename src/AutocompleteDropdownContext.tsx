6ef37674a1ae9bafbb891eb639eae8a5f8253168
windmillcode0 <michaelodumosu57@gmail.com>
2025-02-27 22:01:39 -0500

    [UPDATE] additional work

--- src/AutocompleteDropdownContext.tsx
+++ src/AutocompleteDropdownContext.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useRef, useState, useEffect } from 'react'
 import type { SetStateAction, Dispatch, FC, ReactElement, MutableRefObject } from 'react'
 import type { LayoutChangeEvent, ViewProps } from 'react-native'
-import { StyleSheet, View } from 'react-native'
+import { LayoutAnimation, StyleSheet, View } from 'react-native'
 import type { IAutocompleteDropdownRef } from './types'
 
 export interface IAutocompleteDropdownContext {
@@ -58,6 +58,8 @@ export const AutocompleteDropdownContextProvider: FC<IAutocompleteDropdownContex
       setOpacity(0)
       return
     }
+
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
 
     if (dropdownHeight && direction === 'up') {
       setContentStyles({
